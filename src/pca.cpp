9b8303447c1b16af0c0289fdc0ee4fdad124bb03
Anders Dellien <anders@andersdellien.se>
2016-06-09 21:27:27 +0200

    Adding additional PCA SFR registers

--- src/pca.cpp
+++ src/pca.cpp
@@ -4,7 +4,11 @@
 
 Pca::Pca(Alu &a): alu(a)
 {
+  a.RegisterSfr(0xeb, new Sfr("PCA0CPL2"), 0x00);
+  a.RegisterSfr(0xfa, new Sfr("PCA0H"), 0x00);
+  a.RegisterSfr(0xf9, new Sfr("PCA0L"), 0x00);
   a.RegisterSfr(0xd9, new Sfr("PCA0MD"), 0x00);
+  a.RegisterSfr(0xd8, new Sfr("PCA0CN"), 0x00);
   a.RegisterSfr(0xec, new Sfr("PCA0CPH2"), 0x00);
 }
 

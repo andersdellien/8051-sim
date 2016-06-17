9e65bba67c6841762c892d65de64f4dd90708715
Anders Dellien <anders@andersdellien.se>
2016-06-17 09:06:02 +0200

    Support UART TX interrupt

--- inc/uart.hpp
+++ inc/uart.hpp
@@ -13,14 +13,25 @@ class SBUF0: public Sfr
     void Write(std::uint8_t tx);
 };
 
+class SCON0: public SfrBitAddressable
+{
+  public:
+    SCON0(std::string name, Block &block, std::uint8_t address, std::uint8_t resetValue, std::set<std::uint8_t> pages);
+    void Write(std::uint8_t value);
+    void WriteBit(std::uint8_t bit, bool value);
+};
+
 class Uart: public Block
 {
   public:
     Uart(Alu &alu);
     void SimulateRx(char c);
+    bool interruptPending;
   private:
-    SfrBitAddressable scon0;
+    int CalculateRemainingTicks();
+    void ClockEvent();
     SBUF0 sbuf0;
+    SCON0 scon0;
 };
 
 #endif
--- src/uart.cpp
+++ src/uart.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <set>
 #include "uart.hpp"
 #include "sfr.hpp"
@@ -9,15 +10,45 @@ SBUF0::SBUF0(std::string name, Block &block, std::uint8_t address, std::uint8_t 
 {
 }
 
+SCON0::SCON0(std::string name, Block &block, std::uint8_t address, std::uint8_t resetValue, std::set<std::uint8_t> pages):
+  SfrBitAddressable(name, block, address, resetValue, pages)
+{
+}
+
+#define TI0 2
+#define TI0_BIT 1
+
+void SCON0::Write(std::uint8_t value)
+{
+  if ((data & TI0) && !(value & TI0))
+  {
+    block.ConfigurationChanged();
+  }
+  data = value;
+}
+
+void SCON0::WriteBit(std::uint8_t bit, bool value)
+{
+  if ((data & TI0) && (bit == TI0_BIT) & !value)
+  {
+    block.ConfigurationChanged();
+  }
+  SfrBitAddressable::WriteBit(bit, value);
+}
+
 void SBUF0::Write(std::uint8_t tx)
 {
   std::cout << "UART0 output: " << tx << std::endl;
+  Uart *u = dynamic_cast<Uart*>(&block);
+  u->interruptPending = true;
+  block.ConfigurationChanged();
 }
 
 Uart::Uart(Alu &a) :
   Block(a),
   scon0("SCON0", *this, 0x98, 0x40, {0x0}),
-  sbuf0("SBUF0", *this, 0x99, 0x00, {0x0})
+  sbuf0("SBUF0", *this, 0x99, 0x00, {0x0}),
+  interruptPending(false)
 {
 }
 
@@ -36,4 +67,23 @@ void Uart::SimulateRx(char c)
   {
     std::cout << "UART disabled" << std::endl;
   }
+}
+
+int Uart::CalculateRemainingTicks()
+{
+  if (interruptPending)
+  {
+    interruptPending = false;
+    return 100; // This will work for now, let's calculate a better value later
+  }
+  else
+  {
+    return std::numeric_limits<int>::max();
+  }
+}
+
+void Uart::ClockEvent()
+{
+  scon0.data |= TI0;
+  alu.UartInterrupt();
 }
